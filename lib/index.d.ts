b1596e1c45585a382f76cf5250da05494b132be0
Holly <holly@eusv.net>
2023-05-12 14:41:35 +0100

    delete ^& ignore lib

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import { IReader } from "./readers/IReader";
-import { IWriter } from "./writers/IWriter";
-export declare enum Endian {
-    LE = 0,
-    BE = 1
-}
-export declare function createReader(endianness: Endian, buffer: Buffer): IReader;
-export declare function createWriter(endianness: Endian, size?: number): IWriter;
