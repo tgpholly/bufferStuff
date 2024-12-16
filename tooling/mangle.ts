5c1c4f9a2b70b96ff7ecf10e93d2552afc4f8d08
Holly <onlinegamer748@gmail.com>
2024-12-16 10:23:30 +0000

    1.8.0

--- tooling/mangle.ts
+++ tooling/mangle.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Holly Stubbs (tgpholly) - Licensed under MIT
+// Check LICENSE in repository root for more information.
+
 import { readFileSync, writeFileSync } from "fs";
 import { minify } from "terser";
 
